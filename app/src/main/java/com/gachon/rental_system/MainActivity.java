c5407aa1c1b799e1e6f08b89c9a018dcc3ce5341
Sevng <klatmdgh841@naver.com>
2022-06-01 02:32:32 +0900

    second_version

--- app/src/main/java/com/gachon/rental_system/MainActivity.java
+++ app/src/main/java/com/gachon/rental_system/MainActivity.java
@@ -1,14 +1,61 @@
 package com.gachon.rental_system;
 
+import android.content.Context;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 
 public class MainActivity extends AppCompatActivity {
+    BottomNavigationView bottomNavigationView;
+    com.gachon.rental_system.Fragment1 fragment1;
+    com.gachon.rental_system.Fragment4 fragment4;
+    Fragment5 fragment5;
+
+    public static Context context_main;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        context_main = this;
+
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
+
+        //프래그먼트 생성
+        fragment1 = new com.gachon.rental_system.Fragment1();
+        fragment4= new com.gachon.rental_system.Fragment4();
+        fragment5 = new Fragment5();
+
+        //제일 처음 띄워줄 뷰를 세팅해줍니다. commit();까지 해줘야 합니다.
+        getSupportFragmentManager().beginTransaction().replace(R.id.main_layout,fragment1).commitAllowingStateLoss();
+
+        //bottomnavigationview의 아이콘을 선택 했을때 원하는 프래그먼트가 띄워질 수 있도록 리스너를 추가합니다.
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                switch (menuItem.getItemId()){
+                    //menu_bottom.xml에서 지정해줬던 아이디 값을 받아와서 각 아이디값마다 다른 이벤트를 발생시킵니다.
+                    case R.id.tab1:{
+                        getSupportFragmentManager().beginTransaction().replace(R.id.main_layout,fragment1).commitAllowingStateLoss();
+                        return true;
+                    }
+                    case R.id.tab4:{
+                        getSupportFragmentManager().beginTransaction().replace(R.id.main_layout,fragment4).commitAllowingStateLoss();
+                        return true;
+                    }
+                    case R.id.tab5:{
+                        getSupportFragmentManager().beginTransaction().replace(R.id.main_layout,fragment5).commitAllowingStateLoss();
+                        return true;
+                    }
+                    default:
+                        return false;
+                }
+            }
+        });
     }
 }
